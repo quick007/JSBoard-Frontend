72aec151b2f33229367defb722aa54655acd595b
Lukas <luseufert5@gmail.com>
2021-03-31 03:26:06 +0000

    update some stuff

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const fs = require("fs");
 const app = express();
 app.set("view engine", "ejs");
-app.listen(3000);
+app.listen(3000, ()=>{
+  console.log("Website Loaded");
+});
 app.get("*", async (req, res) =>{
   if(req.path.toLowerCase().endsWith(".css") || req.path.toLowerCase().endsWith(".js")) return res.sendFile(__dirname+"/views"+req.path);
-  else return res.render("."+req.path);
+  else try { return res.render("."+req.path);}catch{}
 })
--- views/script.js
+++ views/script.js
@@ -1,14 +1,2 @@
-function pickoption(){
-  let value = document.getElementById("dbengine").value;
-  if(value==="sql"){
-    document.getElementById("mysql").style.display = "none";
-    document.getElementById("mongodb").style.display = "none";
-  } else if (value==="mongodb"){
-    document.getElementById("mysql").style.display = "none";
-    document.getElementById("mongodb").style.display = "";
-  }else if (value==="mysql"){
-    document.getElementById("mysql").style.display = "";
-    document.getElementById("mongodb").style.display = "none";
-  }
-}
+
 
