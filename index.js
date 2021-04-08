381458387b3ee16e1939d795e6bb7c8bb138df3e
Lukas <>
2021-04-08 00:45:34 +0000

    Upgrade to tailwind JIT

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const fs = require("fs");
 const app = express();
+const themecolor = {
+  bg:"#whatever"
+}
 app.set("view engine", "ejs");
 app.listen(3000, ()=>{
   console.log("Website Loaded");
 });
 app.get("*", async (req, res) =>{
   if(req.path.toLowerCase().endsWith(".css") || req.path.toLowerCase().endsWith(".js")) return res.sendFile(__dirname+"/views"+req.path);
-  else try { return res.render("."+req.path);}catch{}
+  else try { return res.render("."+req.path, {themecolor:themecolor});}catch{}
 })
