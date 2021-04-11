bd54e390c73c02349b0ce2fbfc9f5dc0550f0021
Lukas <>
2021-04-11 02:43:07 +0000

    poggers

--- index.js
+++ index.js
@@ -8,8 +8,12 @@ app.listen(3000, ()=>{
   console.log("Website Loaded");
 });
 app.get("*", async (req, res) =>{
-  if(req.path === "/favicon.ico") return res.send("404");
   themedata = JSON.parse(fs.readFileSync("./config.json", "utf8"));
   if(req.path.toLowerCase().endsWith(".css") || req.path.toLowerCase().endsWith(".js")) return res.sendFile(__dirname+"/views"+req.path);
+  else if (!fs.existsSync("./views"+req.path) && !fs.existsSync("./views"+req.path+".ejs")) return res.render("404")
   else try { return res.render("."+req.path, {theme: themedata});}catch{}
 })
+
+app.use(function(req, res, next){
+  res.status(404).render("404");
+});
